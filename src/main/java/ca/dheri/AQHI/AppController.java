277c4b033e4d1be900c94bc529bb900b939f5da8
Parteek Dheri <ps.dheri@gmail.com>
2023-06-12 03:53:29 -0400

    2

--- src/main/java/ca/dheri/AQHI/AppController.java
+++ src/main/java/ca/dheri/AQHI/AppController.java
@@ -1,6 +1,8 @@
 package ca.dheri.AQHI;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +12,13 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-public class Controller {
+public class AppController {
+    Logger logger = LoggerFactory.getLogger(AppController.class);
+
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        logger.info(principal.getAttributes().toString());
+
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 
