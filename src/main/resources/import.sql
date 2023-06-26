d13bda92110c2969902e0a98327d664fa607ef62
Parteek Dheri <ps.dheri@gmail.com>
2023-06-25 23:43:08 -0400

    forward-headers-strategy

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,1 @@
-INSERT IGNORE INTO `Location` ( `id`, `city` , `name` ) VALUES
-('FALIF', 'Brampton', 'Brampton'),
-('FEUZB', 'Toronto', 'Toronto') ,
-('FDQBU', 'Toronto', 'Toronto EAST'),
-('FCKTB', 'Toronto', 'Toronto WEST'),
-('FDQBX', 'Toronto', 'Toronto North'),
-('FCWYG', 'Toronto', 'Toronto Downtown');
+INSERT IGNORE INTO `Location` ( `id`, `city` , `name` ) VALUES ('FALIF', 'Brampton', 'Brampton'), ('FEUZB', 'Toronto', 'Toronto'), ('FDQBU', 'Toronto', 'Toronto EAST'), ('FCKTB', 'Toronto', 'Toronto WEST'), ('FDQBX', 'Toronto', 'Toronto North'), ('FCWYG', 'Toronto', 'Toronto Downtown');
