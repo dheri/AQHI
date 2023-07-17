630ed6f8fc3022e2de2865bd4935770883318e9c
Parteek Dheri <ps.dheri@gmail.com>
2023-07-17 04:29:29 -0400

    fetch

--- src/main/java/ca/dheri/aqhi/controller/AppController.java
+++ src/main/java/ca/dheri/aqhi/controller/AppController.java
@@ -3,11 +3,14 @@ package ca.dheri.aqhi.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.Map;
@@ -29,5 +32,17 @@ public class AppController {
     }
 
 
+    @GetMapping("/location")
+    public String fetchLocation() {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "https://api.weather.gc.ca/collections/aqhi-forecasts-realtime/items?location_id=FDQBX";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl , String.class);
+        logger.info(response.getStatusCode().toString());
+        logger.info(response.toString());
+        logger.info(response.getBody().toString());
+        return "index";
+    }
 
 }
